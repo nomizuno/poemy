a875f6244f24648b6fbe2fd6a1e15ae8f875e689
mizuno <yuma.m0610@gmail.com>
2018-02-11 10:14:34 +0900

    ユーザーモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 	
+  get 'users/show'
+
   get 'posts/index'
 
    get '' =>"home#top"
@@ -12,6 +14,10 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+
+  
+
+   get "users/:id" => "users#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
