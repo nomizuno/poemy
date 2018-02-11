2af9e849c29aaea741b63ef8f1db5eab48409e92
mizuno <yuma.m0610@gmail.com>
2018-02-11 19:54:23 +0900

    新規投稿を一覧に表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,58 @@ class UsersController < ApplicationController
     end
   end
 
+def edit
+  	@user = User.find_by(id: params[:id])
+  	@user.save
+  end
+
+  def update
+  	@user = User.find_by(id: params[:id])
+  	@user.name = params[:name]
+  	@user.email = params[:email]
+    @user.password = params[:password]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    end
+
+    if @user.save
+    	redirect_to("/users/index")
+      flash[:notice] ="保存できたよ"
+    else
+    	render("/users/edit")
+    end
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice]= "ログインしました"
+      session[:user_id]=@user.id
+
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email =params[:email]
+      @password =params[:password]
+      render("users/login_form")
+    end
+
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to("/login")
+  end
+
+  def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,10 @@ Rails.application.routes.draw do
    post "users/create" => "users#create"
    get "users/:id" => "users#show"
 
+
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
