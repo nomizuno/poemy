3af60ee524954bc91aa4b8aa5b9f2522eff81595
mizuno <yuma.m0610@gmail.com>
2018-02-12 11:19:46 +0900

    ユーザー編集ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   get "users/signup" => "users#new"
   post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
   get "users/:id" => "users#show"
 
 
