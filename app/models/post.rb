84678905b055698c03e4d548ac2194a80c89a919
mizuno <yuma.m0610@gmail.com>
2018-02-12 13:56:53 +0900

    postのバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+   
+  validates :content, {presence: true, length: {maximum: 140},uniqueness: true}
+  validates :user_id, {presence: true}
+  validates :reading, {presence: true, uniqueness: true}
+
 end
