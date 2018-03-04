18669b18297301b4fd05598dfa0574a6567ceae6
mizuno <yuma.m0610@gmail.com>
2018-03-04 22:19:37 +0900

    バリデーション修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
 	
-	 validates :content, {presence: true, length: {maximum: 140},uniqueness: true}
+	 validates :content, {presence: true, length: {maximum: 140},uniqueness: true ,
+	}
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140},uniqueness: true}
   validates :user_id, {presence: true}
   validates :reading, {presence: true, uniqueness: true}
+  
  
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
